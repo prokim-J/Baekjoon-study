19db9efdaeeec96f6eb8a8a0296f2418df5f2693
JungSeok-Kim <rlwajdtjr127@naver.com>
2022-01-03 17:02:51 +0900

    Add files via upload

---
+++ BAEKJOON/src/Baekjoon5/stage1.java
@@ -0,0 +1,19 @@
+package Baekjoon5;
+
+import java.util.*;
+
+public class stage1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] arr = new int[N]; // 1차원 배열 선언
+        
+        for(int i=0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " " + arr[N-1]);
+    }
+}
